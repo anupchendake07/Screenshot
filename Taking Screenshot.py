013e73b2df0e4e2d19a1a7f123fa8038c1defd21
anupchendake07 <89442882+anupchendake07@users.noreply.github.com>
2022-01-17 13:46:48 +0530

    Add files via upload

---
+++ Taking Screenshot.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+screenshot = pyautogui.screenshot()
+
+# Add the path where you want to save the image, & then add .png in ending
+screenshot.save(r'C:\Users\Anup\Pictures\Screenshot.png')
+
